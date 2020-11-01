c67360b26cf24db6a81cb8e52c40aa120618c5ef
nofanmaulana <nofanmaulana1@gmail.com>
2020-11-02 01:28:19 +0700

    first commit

---
+++ app/src/main/java/com/example/k24_test/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.k24_test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=2000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent MainActivity=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(MainActivity);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
